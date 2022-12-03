9a59a55fed9ae0776222ae6e2f0824ef1e2a30e3
ahmed khanfir <ahmed.khanfir.89@gmail.com>
2022-12-03 13:40:14 +0100

    minor changes

--- gen_mutants.sh
+++ gen_mutants.sh
@@ -29,8 +29,8 @@ python3 mbertntcall/mbert_generate_mutants_runner.py \
 -repo_path "$ROOT/test/res/exampleclass/DummyProject" \
 -target_classes src/main/java/example/DummyClass.java \
 -java_home /Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home/ \
--mutated_classes_output_path "$ROOT/test/res/output/smbertnt_mutated_classes/" \
--output_dir "$ROOT/test/res/output/smbertnt_output_dir/" \
+-mutated_classes_output_path "$ROOT/test/res/output/mbertnt_mutated_classes/" \
+-output_dir "$ROOT/test/res/output/mbertnt_output_dir/" \
 # -simple_only True \
 
 
